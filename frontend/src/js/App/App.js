fcacb388046736e9fc7621b937130f79f4058ac7
Daniel Watson <dwatson251@gmail.com>
2021-06-28 17:42:12 +0100

    Initial distribution

---
+++ frontend/src/js/App/App.js
@@ -0,0 +1,15 @@
+import $ from 'jquery';
+import DashboardUserAdd from "../Dashboard/Component/DashboardUserAdd";
+import Dashboard from "../Dashboard/Component/Dashboard";
+
+export default class App
+{
+    constructor()
+    {
+        this.bodyElem = $('body');
+        this.bodyElem.find('.widget.widget-block.dash')
+            .each(function (i, iDashUserCreate) {
+                new Dashboard(iDashUserCreate);
+            });
+    }
+}
